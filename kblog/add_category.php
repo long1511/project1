88c70c68a2851c07e80d6dcff4e50960c65c883a
LongTH2001 <huymai835@gmail.com>
2023-08-29 20:52:37 +0700

    format code

--- kblog/add_category.php
+++ kblog/add_category.php
@@ -24,166 +24,179 @@ if(isset($_POST['name'])){
 <!--[if lt IE 8 ]><html class="no-js ie ie7" lang="en"> <![endif]-->
 <!--[if IE 8 ]><html class="no-js ie ie8" lang="en"> <![endif]-->
 <!--[if IE 9 ]><html class="no-js ie ie9" lang="en"> <![endif]-->
-<!--[if (gte IE 8)|!(IE)]><!--><html class="no-js" lang="en"> <!--<![endif]-->
+<!--[if (gte IE 8)|!(IE)]><!-->
+<html class="no-js" lang="en">
+<!--<![endif]-->
+
 <head>
 
-   <!--- Basic Page Needs
+    <!--- Basic Page Needs
    ================================================== -->
-   <meta charset="utf-8">
-	<title>K!</title>
-	<meta name="description" content="">  
-	<meta name="author" content="">
+    <meta charset="utf-8">
+    <title>K!</title>
+    <meta name="description" content="">
+    <meta name="author" content="">
 
-	<!-- mobile specific metas
+    <!-- mobile specific metas
    ================================================== -->
-	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 
-   <!-- CSS
+    <!-- CSS
     ================================================== -->
-   <link rel="stylesheet" href="css/default.css">
-	<link rel="stylesheet" href="css/layout.css">  
-	<link rel="stylesheet" href="css/media-queries.css"> 
+    <link rel="stylesheet" href="css/default.css">
+    <link rel="stylesheet" href="css/layout.css">
+    <link rel="stylesheet" href="css/media-queries.css">
 
-   <!-- Script
+    <!-- Script
    ================================================== -->
-	<script src="js/modernizr.js"></script>
+    <script src="js/modernizr.js"></script>
 
-   <!-- Favicons
+    <!-- Favicons
 	================================================== -->
-	<link rel="shortcut icon" href="favicon.png" >
+    <link rel="shortcut icon" href="favicon.png">
 
 </head>
 
 <body>
 
-   <!-- Header
+    <!-- Header
    ================================================== -->
-   <header id="top">
+    <header id="top">
 
-   	<div class="row">
+        <div class="row">
 
-   		<div class="header-content twelve columns">
+            <div class="header-content twelve columns">
 
-		      <h1 id="logo-text"><a href="index.php" title="">K!</a></h1>
-				<p id="intro">You want to be at my level? CLIMB BITCH!</p>
+                <h1 id="logo-text"><a href="index.php" title="">K!</a></h1>
+                <p id="intro">You want to be at my level? CLIMB BITCH!</p>
 
-			</div>			
+            </div>
 
-	   </div>
+        </div>
 
-	   <nav id="nav-wrap"> 
+        <nav id="nav-wrap">
 
-	   	<a class="mobile-btn" href="#nav-wrap" title="Show navigation">Show Menu</a>
-		   <a class="mobile-btn" href="#" title="Hide navigation">Hide Menu</a>
+            <a class="mobile-btn" href="#nav-wrap" title="Show navigation">Show Menu</a>
+            <a class="mobile-btn" href="#" title="Hide navigation">Hide Menu</a>
 
-	   	<div class="row">    		            
+            <div class="row">
 
-			   	<ul id="nav" class="nav">
-			      	<li class="current"><a href="index.php">Home</a></li>    	
-			      	<li><a href="page.html">About</a></li>
-			   	</ul> <!-- end #nav -->			   	 
+                <ul id="nav" class="nav">
+                    <li class="current"><a href="index.php">Home</a></li>
+                    <li><a href="page.html">About</a></li>
+                </ul> <!-- end #nav -->
 
-	   	</div> 
+            </div>
 
-	   </nav> <!-- end #nav-wrap --> 	     
+        </nav> <!-- end #nav-wrap -->
 
-   </header> <!-- Header End -->
+    </header> <!-- Header End -->
 
-   <!-- Content
+    <!-- Content
    ================================================== -->
-   <div id="content-wrap">
+    <div id="content-wrap">
 
-   	<div class="row">
+        <div class="row">
 
-   		<div id="main" class="eight columns">
+            <div id="main" class="eight columns">
 
-	   		<article class="entry">
-					<header class="entry-header">
-	     <?php if(isset($error)){
+                <article class="entry">
+                    <header class="entry-header">
+                        <?php if(isset($error)){
             echo $error;
             } ?>
-						<h2 class="entry-title">
-							<h2>Add Category</h2>
-						</h2> 				 
-					
-						<div class="entry-meta">
-		<form action='' method='post'>
-        <label for='name'>Name </label>
-        <input type='text' name='name' />
-        <input type='submit' value='Add Category' />
-		</form>
-						</div> 
-					 
-					</header> 
-	
-					
-					<div class="entry-content">
-						<p><!--insert here--></p>
-					</div> 
+                        <h2 class="entry-title">
+                            <h2>Add Category</h2>
+                        </h2>
+
+                        <div class="entry-meta">
+                            <form action='' method='post'>
+                                <label for='name'>Name </label>
+                                <input type='text' name='name' />
+                                <input type='submit' value='Add Category' />
+                            </form>
+                        </div>
+
+                    </header>
 
 
-				</article> <!-- end entry -->
+                    <div class="entry-content">
+                        <p>
+                            <!--insert here-->
+                        </p>
+                    </div>
 
-   		</div> <!-- end main -->
 
-   		<div id="sidebar" class="four columns">
+                </article> <!-- end entry -->
 
-   			<div class="widget widget_search">
-                  <h3>Search</h3> 
-                  <form action="#">
+            </div> <!-- end main -->
 
-                     <input type="text" value="Search here..." onblur="if(this.value == '') { this.value = 'Search here...'; }" onfocus="if (this.value == 'Search here...') { this.value = ''; }" class="text-search">
-                     <input type="submit" value="" class="submit-search">
+            <div id="sidebar" class="four columns">
 
-                  </form>
-               </div>
+                <div class="widget widget_search">
+                    <h3>Search</h3>
+                    <form action="#">
 
-   			<div class="widget widget_categories group">
-   				<h3>Categories.</h3> 
-   				<?php
+                        <input type="text" value="Search here..."
+                            onblur="if(this.value == '') { this.value = 'Search here...'; }"
+                            onfocus="if (this.value == 'Search here...') { this.value = ''; }" class="text-search">
+                        <input type="submit" value="" class="submit-search">
+
+                    </form>
+                </div>
+
+                <div class="widget widget_categories group">
+                    <h3>Categories.</h3>
+                    <?php
      foreach(get_categories() as $category){
      ?>
-      <p><a href="manage_category.php?id=<?php echo $category['id'];?>"><?php echo $category['name']; ?></a>
-     <?php  
+                    <p><a
+                            href="manage_category.php?id=<?php echo $category['id'];?>"><?php echo $category['name']; ?></a>
+                        <?php  
      }
      ?>
-				</div>
+                </div>
 
-				<div class="widget widget_text group">
-					<h3>Daily Quote of the Day</h3>
-   				<p>What is success?</p><br/>
-				<p>"Success is not final; failure is not fatal: It is the courage to continue that counts." - Winston S. Churchill</p>
+                <div class="widget widget_text group">
+                    <h3>Daily Quote of the Day</h3>
+                    <p>What is success?</p><br />
+                    <p>"Success is not final; failure is not fatal: It is the courage to continue that counts." -
+                        Winston S. Churchill</p>
 
-   			</div>
-   			
-   		</div> <!-- end sidebar -->
+                </div>
 
-   	</div> <!-- end row -->
+            </div> <!-- end sidebar -->
 
-   </div> <!-- end content-wrap -->
-   
+        </div> <!-- end row -->
 
-   <!-- Footer
+    </div> <!-- end content-wrap -->
+
+
+    <!-- Footer
    ================================================== -->
-   <footer>
+    <footer>
 
-      <div class="row"> 
+        <div class="row">
 
-         <p class="copyright">&copy; Copyright 2017. The Blog. &nbsp; Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a>.</p>
-        
-      </div> <!-- End row -->
+            <p class="copyright">&copy; Copyright 2017. The Blog. &nbsp; Design by <a title="Styleshout"
+                    href="http://www.styleshout.com/">Styleshout</a>.</p>
 
-      <div id="go-top"><a class="smoothscroll" title="Back to Top" href="#top"><i class="fa fa-chevron-up"></i></a></div>
+        </div> <!-- End row -->
 
-   </footer> <!-- End Footer-->
+        <div id="go-top"><a class="smoothscroll" title="Back to Top" href="#top"><i class="fa fa-chevron-up"></i></a>
+        </div>
+
+    </footer> <!-- End Footer-->
 
 
-   <!-- Java Script
+    <!-- Java Script
    ================================================== -->
-   <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-   <script>window.jQuery || document.write('<script src="js/jquery-1.10.2.min.js"><\/script>')</script>
-   <script type="text/javascript" src="js/jquery-migrate-1.2.1.min.js"></script>  
-   <script src="js/main.js"></script>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    <script>
+    window.jQuery || document.write('<script src="js/jquery-1.10.2.min.js"><\/script>')
+    </script>
+    <script type="text/javascript" src="js/jquery-migrate-1.2.1.min.js"></script>
+    <script src="js/main.js"></script>
 
 </body>
 
